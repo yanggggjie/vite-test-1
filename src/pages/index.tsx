9736d85cb1313bd35dc96dd6fc1f82332a6c815e
yanggggjie <60206592+yanggggjie@users.noreply.github.com>
2024-12-24 21:16:38 +0800

    asfsd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import Test from '@/pages/Test.tsx'
 interface Props {}
 
 export default function Index({}: Props) {
   return (
     <div>
+      12313
       <Test></Test>
     </div>
   )
